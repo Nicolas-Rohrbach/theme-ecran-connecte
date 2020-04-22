a5387364d03e00e2a9907e5e39960136d1500502
Nicolas-Rohrbach <nicolas.rohrbach@etu.univ-amu.fr>
2020-04-22 09:38:56 +0200

    Remise au propre du thème - Revoir la customisation / affichage événement

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
-<?php
-
-echo '
+<?php ?>
 <!-- FOOTER -->
-<footer>
-    <div class="footer_wp">';
-        wp_footer();
-echo '
-    </div>
-</footer>';
+<footer class="footer_ecran">
+    <p class="text-center">© 2020 - Aix-Marseille Université</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,8 @@
-<?php
-
-$current_user = wp_get_current_user();
-if (is_user_logged_in() && !in_array("technicien", $current_user->roles)) {
-    if( get_theme_mod( 'sidebar_right_display', 'show' ) == 'show' ) {
-	    echo '<div class="sidebar" id="sidebar-right">';
-            if (! function_exists('dynamic_sidebar') || ! dynamic_sidebar('Colonne Droite')) {
-		    the_widget('WidgetInformation');
-	    }
-        echo '</div>';
-    }
-}
+<?php if(get_theme_mod('sidebar_right_display', 'show') == 'show') : ?>
+    <!-- SIDEBAR -->
+    <aside id="sidebar" class="col-md-4 order-md-2 text-center sidebar" role="complementary">
+        <?php if (! function_exists('dynamic_sidebar') || ! dynamic_sidebar('Colonne Droite')) :
+            the_widget('WidgetInformation');
+        endif; ?>
+    </aside>
+<?php endif; ?>
